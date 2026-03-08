e3eefbfe90c18e3b29824360a133a1ecfe7808fd
Miles King <kingmilest@gmail.com>
2026-03-08 19:28:51 -0400

    Added successful creation print message to sqlite table creation script.

--- src/ingestion/create_table.py
+++ src/ingestion/create_table.py
@@ -2,15 +2,22 @@ import sqlite3
 
 connection = sqlite3.connect('chess_data.db')
 cursor = connection.cursor()
-connection.execute("CREATE TABLE games("
-                   "id INTEGER PRIMARY KEY, "
-                   "site_id INTEGER NOT NULL, "
-                   "date TEXT, "
-                   "white_id INTEGER, "
-                   "black_id INTEGER, "
-                   "white_elo INTEGER NOT NULL, "
-                   "black_elo INTEGER NOT NULL, "
-                   "result TEXT NOT NULL, "
-                   "time_control TEXT NOT NULL, "
-                   "opening_ECO TEXT, "
-                   "moves BLOB NOT NULL)")
+try:
+    connection.execute("DROP TABLE games")
+    connection.execute("CREATE TABLE games("
+                       "id INTEGER PRIMARY KEY, "
+                       "site_id INTEGER NOT NULL, "
+                       "date TEXT, "
+                       "white_id INTEGER, "
+                       "black_id INTEGER, "
+                       "white_elo INTEGER NOT NULL, "
+                       "black_elo INTEGER NOT NULL, "
+                       "result TEXT NOT NULL, "
+                       "termination_condition TEXT NOT NULL, "
+                       "time_control TEXT NOT NULL, "
+                       "opening_ECO TEXT, "
+                       "moves BLOB NOT NULL)")
+except Exception as e:
+    print(e)
+else:
+    print("Table created successfully")
